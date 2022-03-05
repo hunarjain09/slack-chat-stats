ec0e966b9350ddb7dc08caf45e896c137ac2b82c
vasanth.kanugo <vasanth.kanugo@gmail.com>
2022-03-05 16:48:31 -0700

    changes for token

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from slack_sdk.errors import SlackApiError
 
 
 app = Flask(__name__)
-
-@app.route('/', method=['POST'])
+auth_token = 'xoxb-3210173726865-3200014683316-nTkYQs8qDmjNe3R3X0eC81M3'
+@app.route('/slack', methods=['POST'])
 def hello_world():
     req_data = request.form
-    client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+    client = WebClient(token=auth_token)
 
     logger = logging.getLogger(__name__)
     conversation_history = []
@@ -18,18 +18,22 @@ def hello_world():
     channel_id = req_data.getlist('channel_id')
     try:
         # last 100 conversations
-        result = client.conversations_history(channel=channel_id, limit=100)
+        result = client.conversations_history(channel=channel_id[0], limit=100)
         messages = result["messages"]
         users = set()
         for message in messages:
             # unique users in the conversation
-            users.add(message['user'])
-            # create a conversation history of user and message
-            conversation_history.append((message['user'], message['text'].decode('utf-8')))
+            if 'user' in message:
+                users.add(message['user'])
+                # create a conversation history of user and message
+                conversation_history.append((message['user'], message['text']))
         # call NLP library
         logger.info("{} messages found in {}".format(len(conversation_history), id))
     except SlackApiError as e:
         logger.error("Error creating conversation: {}".format(e))
 
-    return 'Hello Sammy!'
+    return str(conversation_history)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8888)
 
